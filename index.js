d5d8480f4570c56df91e727b62aa519e3a479914
George Baroi <baroi.george@gmail.com>
2022-06-17 23:34:56 -0400

    commit tree e0cdecc274abe860a34e7a5a279770373119d47e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+let employee = {}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+return { name : "Sam" , 
+         streetAddress : "11 Broadway"}
+}
+ 
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee["streetAddress"] = "12 Broadway"
+    return employee }
+
+function deleteFromEmployeeByKey(employee, key , value){
+let newEmployee = {...employee}
+newEmployee[key] = value
+return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee , key , value){
+    employee[key] = value
+    return employee 
+}
